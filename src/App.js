64766e632581a5a8d9bc9bff896597baaadc0519
RL20 <harel_shalom@yahoo.com>
2022-09-14 14:46:13 +0300

    react and php basic connction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import { useState } from "react";
+import $ from "jquery";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    const form = $(e.target);
+    $.ajax({
+      type: "POST",
+      url: form.attr("action"),
+      data: form.serialize(),
+      success(data) {
+        setResult(data);
+      },
+    });
+  };
+  //able to run different ports for example http://localhost:8000/server.php, but have to configure xampp to run the server on 8000 port
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="http://localhost/server.php" method="post" onSubmit={(event) => handleSumbit(event)}>
+        <label htmlFor="name">Name: </label>
+        <input type="text" id="name" name="name" value={name} onChange={(event) => handleChange(event)} />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      <h1>{result}</h1>
     </div>
   );
 }
